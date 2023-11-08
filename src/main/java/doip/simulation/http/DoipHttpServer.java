c25e34dbf9eae97928899e71509997fe71d9fb0e
Marco Wehnert <marco.wehnert@gmx.de>
2023-11-08 22:17:20 +0000

    Added first essential files

---
+++ src/main/java/doip/simulation/http/DoipHttpServer.java
@@ -0,0 +1,14 @@
+package doip.simulation.http;
+
+import doip.simulation.api.SimulationManager;
+
+public class DoipHttpServer {
+	
+	private SimulationManager simulationManager = null;
+	
+	public DoipHttpServer(SimulationManager simulationManager) {
+		this.simulationManager = simulationManager;
+	}
+
+}
+
