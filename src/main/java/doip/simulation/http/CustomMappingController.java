fdc3384ba3cd109342a3fc353bf0528f79ebce1d
npyvovar <nataliya.pyvovar@expleogroup.com>
2023-11-10 12:20:37 +0100

    Extended CustomMappingController

--- src/main/java/doip/simulation/http/CustomMappingController.java
+++ src/main/java/doip/simulation/http/CustomMappingController.java
@@ -1,6 +1,8 @@
 package doip.simulation.http;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -20,7 +22,17 @@ public class CustomMappingController {
 
 	private static Logger logger = LogManager.getLogger(CustomMappingController.class);
 
-	public DoipHttpServer httpServer;
+	private final DoipHttpServer httpServer;
+
+	/**
+	 * Constructs a CustomMappingController with the specified DoipHttpServer
+	 * instance.
+	 *
+	 * @param httpServer The DoipHttpServer instance to use.
+	 */
+	public CustomMappingController(DoipHttpServer httpServer) {
+		this.httpServer = httpServer;
+	}
 
 	/**
 	 * Starts an HTTP server with custom POST and GET mappings.
@@ -30,17 +42,18 @@ public class CustomMappingController {
 	 */
 	public void startHttpServer(SimulationManager simulationManager) {
 		try {
-
+			// Create custom POST and GET handlers
 			HttpHandler postHandler = new PostHandlerCustom();
 			HttpHandler getHandler = new GetHandlerCustom();
 
+			// Create custom mapping contexts
 			List<ContextHandler> customHandlers = List.of(new ContextHandler("/customPost", postHandler),
 					new ContextHandler("/customGet", getHandler));
 
-			httpServer = new DoipHttpServer(simulationManager);
+			// Create and configure the DoipHttpServer instance
 			httpServer.createMappingContexts(customHandlers);
 			httpServer.start();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			logger.error("Error starting the server: {}", e.getMessage(), e);
 		}
 	}
@@ -52,24 +65,39 @@ public class CustomMappingController {
 
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
-			// TODO Auto-generated method stub
-
+			// TODO: Implement custom logic for handling POST requests
+			// Read request body, process data, and generate response
+			String response = "Custom POST request processed.";
+			sendResponse(exchange, response);
 		}
-		// ... (PostHandler implementation)
 	}
 
 	/**
 	 * Custom handler for processing GET requests in a custom context.
 	 */
-
 	class GetHandlerCustom implements HttpHandler {
 
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
-			// TODO Auto-generated method stub
-
+			// TODO: Implement custom logic for handling GET requests
+			// Process parameters, and generate response
+			String response = "Custom GET request processed.";
+			sendResponse(exchange, response);
 		}
-		// ... (GetHandler implementation)
 	}
 
+	/**
+	 * Sends a response to the client with the given message.
+	 *
+	 * @param exchange The HTTP exchange.
+	 * @param message  The message to send in the response.
+	 * @throws IOException If an I/O error occurs while sending the response.
+	 */
+	private void sendResponse(HttpExchange exchange, String message) throws IOException {
+		exchange.getResponseHeaders().add("Content-Type", "text/plain");
+		exchange.sendResponseHeaders(200, message.getBytes(StandardCharsets.UTF_8).length);
+		try (OutputStream responseBody = exchange.getResponseBody()) {
+			responseBody.write(message.getBytes(StandardCharsets.UTF_8));
+		}
+	}
 }
